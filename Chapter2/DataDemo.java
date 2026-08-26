a54cab086ba50a13abe421b5f3216702045957af
VictorDelgadoJ-Ops <javiidel11@gmail.com>
2026-08-26 14:32:02 -0400

    initial program to demonstrate the use of data types

---
+++ Chapter2/DataDemo.java
@@ -0,0 +1,14 @@
+// Victor Delgado
+// P. 45
+
+package Chapter2;
+
+public class DataDemo
+{
+	public static void main(String[] args)
+	{
+		int aWholeNumber = 315;
+		System.out.print("The number is ");
+		System.out.println(aWholeNumber);
+	}
+}
